b153aa7f026b7fb18c11f7b0d99a55305c9f9a8b
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-03 00:51:25 +0300

    added urls.py for navigating

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('listings.urls')),  # include your app's urls
+    path('swagger/', include_docs_urls(title='ALX Travel API')),  # Optional Swagger UI
 ]
